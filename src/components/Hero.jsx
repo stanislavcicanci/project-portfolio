997c8b45ddb7d92b5e33404e033e36a830412623
Stanislav Cicanci <stanislavacicanci@gmail.com>
2024-04-13 11:48:01 +0300

    incercare rezolgare gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 import { useGSAP } from "@gsap/react";
-import gsap from 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js';
+import gsap from 'gsap';
 
 const Hero = () => {
     const textRef = useRef(null);
