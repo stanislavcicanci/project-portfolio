94f18c3bb0a550328186cebd1da333bfb984849b
Stanislav Cicanci <stanislavacicanci@gmail.com>
2024-04-20 14:27:42 +0300

    textul normal reverificat

---
+++ src/components/Section3home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Section3home = () => {
+return (
+    <div>
+        <div className="pt-[9.50rem]">
+            
+        </div>     
+    </div>
+)
+}
+
+export default Section3home
