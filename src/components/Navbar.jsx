2fa748cdb2b646d219033f9228f414aff84365ed
Stanislav Cicanci <stanislavacicanci@gmail.com>
2024-04-22 20:14:45 +0300

    sectiunea 3 home gata

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
 return (
     <div>
-        <nav className='h-[90px] border-b-[3px] border-zinc-100 bg-[#121212] text-white z-50'>
+        <nav className='h-[90px] border-b-[1px] border-zinc-100 order-opacity-50 bg-[#121212] text-white z-50'>
         <div className="flex justify-between leading-[18px] text-lg font-medium pt-[2.5rem] pr-[8rem] pl-[8rem] flex-wrap">
         <div className="logo">
                 CĂTĂLIN ȚURKANU.
