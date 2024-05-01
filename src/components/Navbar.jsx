b535a2a69be069b8a5bb7bdae8cf4cb171727ea4
Cicanci Stanislav <stanislavacicanci@gmail.com>
2024-05-01 09:57:38 +0300

    Linia la nav transoarenta

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 
 const Navbar = () => {
+    // scroll opacity
+    const [hrOpacity, setHrOpacity] = useState(0.5);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const newOpacity = Math.max(0, Math.min(1, 0.5 - scrollTop / 100));
+        setHrOpacity(newOpacity);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+        window.removeEventListener("scroll", handleScroll);
+    };
+    }, []);
+
 return (
     <div>
         <nav className='fixed mix-blend-difference top-0 left-0 right-0 h-[90px] text-white z-50'>
@@ -17,7 +34,12 @@ return (
             </div>
         </div>
         </nav>
-        <hr className='absolute w-[100vw] h-[1px] text-zinc-100 top-[90px] opacity-50 z-[999]' /> 
+        <hr
+        className="absolute w-[100vw] h-[1px] text-zinc-100 top-[90px] opacity-50 z-[999]"
+        style={{ opacity: hrOpacity }}
+        transition={{ duration: 0.01 }}
+        />
+
     </div>
 )
 }
