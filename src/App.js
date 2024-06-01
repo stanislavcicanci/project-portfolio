a07e0e8fedc0ca763738745339e7ad9d971fb90e
Stanislav Cicanci <stanislavacicanci@gmail.com>
2024-06-01 13:49:15 +0300

    Home content responsive

--- src/App.js
+++ src/App.js
@@ -128,8 +128,8 @@ function App() {
           <Navbar />
           <Hero />
           <HomeContent />
-          {/* <Section3home /> */}
-          {/* <Footer /> */}
+          <Section3home />
+          <Footer />
         </div>
       </SmoothScroll>
     </>
