4d037a188308d74cbd1dd79556ed011cb763e78b
Stanislav Cicanci <stanislavacicanci@gmail.com>
2024-06-06 19:31:30 +0300

    Incercare la bara de sus

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import work001 from './images/work001.svg';
-import work002 from './images/work002.svg';
-import work003 from './images/work003.svg';
+import work001 from './images/work001.png';
+import work002 from './images/work002.png';
+import work003 from './images/work003.png';
 import plus from './images/plus.svg';
 
 
